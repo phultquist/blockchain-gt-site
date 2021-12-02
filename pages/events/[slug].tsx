d0c43bdd259ff81823595fec3c6d96225c5569db
Patrick Hultquist <patrick@patrick.today>
2021-12-01 23:53:39 -0500

    events page

--- pages/events/[slug].tsx
+++ pages/events/[slug].tsx
@@ -1,57 +1,45 @@
 import { Navbar, Header } from "../../components";
 import type { NextPage } from "next";
-
-const events = [
-  {
-    title: "Blockchain Hackathon",
-    date: "2020-03-01",
-    description:
-      "A hackathon for students to learn about blockchain and blockchain technology.",
-    link: "https://www.facebook.com/events/2345678901234567",
-  },
-  {
-    title: "Blockchain Hackathon",
-    date: "2020-03-02",
-    description:
-      "A hackathon for students to learn about blockchain and blockchain technology.",
-    link: "https://www.facebook.com/events/2345678901234567",
-  },
-  {
-    title: "Blockchain Hackathon",
-    date: "2020-03-03",
-    description:
-      "A hackathon for students to learn about blockchain and blockchain technology.",
-    link: "https://www.facebook.com/events/2345678901234567",
-  },
-];
+import events from "./events"
+import Head from "next/head"
+import { useRouter } from "next/router";
 
 const Events: NextPage = () => {
+  const path = useRouter().asPath;
+  const event = events.find(e => e.slug === path.split("/")[2]);
+  console.log(path);
+
   return (
     <div className="bg-gray-50 min-h-screen">
+      <Head>
+        <title>{event.title}</title>
+      </Head>
       <Navbar />
-      <Header title="Upcoming Events" />
+      <Header title={event.title} />
       <div className="container max-w-screen-lg mx-auto py-6 px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-          {events.map((event) => (
-            <div
-              key={event.date + event.title}
-              className="group bg-white rounded-lg shadow-md p-6 hover:shadow-lg hover:scale-[1.02] transition-all cursor-pointer"
-            >
-              <h2 className="text-2xl font-bold">Event Detail</h2>
-              <p className="text-gray-600 text-sm mb-2">{event.date}</p>
-              <p className="text-gray-600 mb-2">{event.description}</p>
-              <a href={event.link} className="text-primary font-semibold">
-                Learn More{" "}
-                <span className="group-hover:opacity-100 opacity-0 group-hover:translate-x-0 -translate-x-10 transition-all font-light">
-                  &rarr;
-                </span>
-              </a>
-            </div>
-          ))}
+        <div className="">
+          <h1 className="text-2xl">{event.description}</h1>
+          <h1 className="text-2xl">{event.link}</h1>
+          <h1 className="text-2xl">{event.date}</h1>
         </div>
       </div>
     </div>
   );
 };
+
+export function getStaticProps(context) {
+  return {
+    props: {
+      // event: events.find(e => e.slug === context.params.slug)
+    }
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: events.map(e => `/events/${e.slug}`),
+    fallback: true
+  };
+}
 
 export default Events;
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,29 +1,7 @@
 import { Navbar, Header } from "../../components";
 import type { NextPage } from "next";
-
-const events = [
-  {
-    title: "Blockchain Hackathon",
-    date: "2020-03-01",
-    description:
-      "A hackathon for students to learn about blockchain and blockchain technology.",
-    link: "https://www.facebook.com/events/2345678901234567",
-  },
-  {
-    title: "Blockchain Hackathon",
-    date: "2020-03-02",
-    description:
-      "A hackathon for students to learn about blockchain and blockchain technology.",
-    link: "https://www.facebook.com/events/2345678901234567",
-  },
-  {
-    title: "Blockchain Hackathon",
-    date: "2020-03-03",
-    description:
-      "A hackathon for students to learn about blockchain and blockchain technology.",
-    link: "https://www.facebook.com/events/2345678901234567",
-  },
-];
+import events from "./events"
+import Link from "next/link"
 
 const Events: NextPage = () => {
   return (
@@ -33,20 +11,25 @@ const Events: NextPage = () => {
       <div className="container max-w-screen-lg mx-auto py-6 px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
           {events.map((event) => (
-            <div
+            <Link
               key={event.date + event.title}
-              className="group bg-white rounded-lg shadow-md p-6 hover:shadow-lg hover:scale-[1.02] transition-all cursor-pointer"
+              href={'/events/' + event.slug}
+              passHref
             >
-              <h2 className="text-2xl font-bold">{event.title}</h2>
-              <p className="text-gray-600 text-sm mb-2">{event.date}</p>
-              <p className="text-gray-600 mb-2">{event.description}</p>
-              <a href={event.link} className="text-primary font-semibold">
-                Learn More{" "}
-                <span className="group-hover:opacity-100 opacity-0 group-hover:translate-x-0 -translate-x-10 transition-all font-light">
-                  &rarr;
-                </span>
-              </a>
-            </div>
+              <div
+                className="group bg-white rounded-lg shadow-md p-6 hover:shadow-lg hover:scale-[1.02] transition-all cursor-pointer"
+              >
+                <h2 className="text-2xl font-bold">{event.title}</h2>
+                <p className="text-gray-600 text-sm mb-2">{event.date}</p>
+                <p className="text-gray-600 mb-2">{event.description}</p>
+                <a href={event.link} className="text-primary font-semibold">
+                  Learn More{" "}
+                  <span className="group-hover:opacity-100 opacity-0 group-hover:translate-x-0 -translate-x-10 transition-all font-light">
+                    &rarr;
+                  </span>
+                </a>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
